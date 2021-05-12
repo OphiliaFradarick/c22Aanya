9875634f728bfa6ec4440d0d03bfc690a405e9c4
OphiliaFradarick <76613993+OphiliaFradarick@users.noreply.github.com>
2021-05-12 08:03:46 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,32 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+
+var canvas, engine, world
+
+function setup(){
+
+  canvas = createCanvas(400,400);
+  engine = Engine.create();
+  world = engine.world;
+
+  var options = {
+    isStatic : true
+  }
+
+  ground = Bodies.rectangle(200,380,400,10,options);
+  World.add(world, ground);
+
+
+}
+
+function draw(){
+  background(0);
+  Engine.update(engine);
+
+  fill("hotpink");
+  rectMode(CENTER);
+  rect(ground.position.x, ground.position.y, 400,10);
+
+
+}
